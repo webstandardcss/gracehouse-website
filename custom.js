881ce7983719d1e7f6427653a11942386dc9b53f
webstandardcss <webstandardcss@gmail.com>
2018-11-20 03:19:36 -0600

    fix spelling error in code, did not affect functionality

--- custom.js
+++ custom.js
@@ -7,9 +7,9 @@
       [].slice.call(parallax).forEach(function(el,i){
   
         var windowYOffset = window.pageYOffset,
-            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+            elBackgroundPos = "50% " + (windowYOffset * speed) + "px";
   
-        el.style.backgroundPosition = elBackgrounPos;
+        el.style.backgroundPosition = elBackgroundPos;
   
       });
     };
