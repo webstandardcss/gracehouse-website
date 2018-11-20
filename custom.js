b82a0291298f0dd84feaa0e4b3582baf290b9d06
webstandardcss <webstandardcss@gmail.com>
2018-11-20 04:52:56 -0600

    start mobile menu

--- custom.js
+++ custom.js
@@ -14,10 +14,10 @@
       });
     };
 
-    $(".grace-menu").click(function() {
-        window.location = $(this).find("a").attr("href"); 
-        return false;
-    });    
+    var menuContainer = document.querySelectorAll(".bar-menu-mobile");
+    var menuSelector = document.querySelectorAll(".bar-menu");
+    
+    menuContainer.innerHTML = menuSelector.innerHTML;
 
   })();
 
@@ -25,3 +25,26 @@
 
 
   
+  $(function() {
+
+    menuMobile = $(".bar-menu-mobile");
+    menuContent = $(".bar-menu");
+    menuButtonShow = $(".navbar .btn-action");
+    menuButtonHide = $(".bar-menu-mobile .icon-cross");
+
+
+
+    menuMobile.hide();  
+    menuButtonShow.click(function() {
+        menuMobile.show();    
+    });    
+
+    menuButtonHide.click(function() {
+        menuMobile.hide();    
+    });    
+
+    $(".grace-menu").click(function() {
+        window.location = $(this).find("a").attr("href"); 
+        return false;
+    });    
+});
