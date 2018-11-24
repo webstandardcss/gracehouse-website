a328258d52fef6572439ad9e956eb23eabebfaa5
webstandardcss <webstandardcss@gmail.com>
2018-11-24 15:06:18 -0600

    education.php

---
+++ education.php
@@ -0,0 +1,240 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "  " . $pageTitle;
+$pageDescription = $siteName  . "  " . $pageTitle;
+$pageKeywords = $pageTitle . "  " . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+
+    <!-- blank.php -->
+    <div class="home-banner flex-centered">
+        <div class="home-banner-inner">
+            <h5>
+                &ldquo;&nbsp;&nbsp;&rdquo;
+                &mdash; 
+            </h5>
+        </div>
+    </div>
+
+    <div class="container bar-content p-centered page-width">    
+        <div class="columns">
+            <div class="column col-8 col-md-12 col-mx-auto">
+                <h2 class="text-center">Education and Recovery Classes</h2>
+                <p>
+                Grace House Education Classes:
+                </p>
+
+                <dl class="links-education">
+                <dt>
+                  Disciple Daily
+                </dt>
+
+                <dd>
+                  Learning 12 familiar stories of the Bible and how to tell them in a memorable way to any audience.
+                </dd>
+
+                <dt>
+                  Boundaries
+                </dt>
+
+                <dd>
+                  A book study which helps one determine healthy and unhealthy boundaries in relationships and life situations.
+                </dd>
+
+                <dt>
+                  Celebrate Recovery
+                </dt>
+
+                <dd>
+                  An ongoing group meeting focusing on addiction recovery.
+                </dd>
+
+                <dt>
+                  Life’s Healing Choices
+                </dt>
+
+                <dd>
+                  Is a faith based AA/NA equivalent recovery program that assists in overcoming addictions and encourages clean and sober living.
+                </dd>
+
+                <dt>
+                  Founder’s Foundations
+                </dt>
+
+                <dd>
+                  Class offering Biblical views on various topics relevant to today’s society.
+                </dd>
+
+                <dt>
+                  Celebration Choir
+                </dt>
+
+                <dd>
+                  Participating in special choir events. This includes weekly practices and presentation.
+                </dd>
+
+                <dt>
+                  Experiencing God
+                </dt>
+
+                <dd>
+                  A Bible study course on how to hear from God, know His will  and experience His goodness.
+                </dd>
+
+                <dt>
+                  Guest Speakers
+                </dt>
+
+                <dd>
+                  An assortment of community leaders and ministers visit Grace House and share their life stories.
+                </dd>
+
+                <dt>
+                  Art For the Heart
+                </dt>
+
+                <dd>
+                  Art classes that include crafts, painting, beading, and more.
+                </dd>
+
+                <dt>
+                  Financial Peace
+                </dt>
+
+                <dd>
+                  Study that gives basic instruction on money management and finances.
+                </dd>
+
+                <dt>
+                  Nutrition
+                </dt>
+
+                <dd>
+                  Texas A&M Agri-Life Extension Service- Expanded Food and Nutrition Education Program in Bexar County.
+                </dd>
+
+                <dt>
+                  Safe Food Handling Certificate
+                </dt>
+
+                <dd>
+                  Online course required by SA Food Bank recipients. Online Course
+                </dd>
+
+                <dt>
+                  Communication/Connection/Growth
+                </dt>
+
+                <dd>
+                  Learning to communicate, connect, and grow in healthy relationships.
+                </dd>
+
+                <dt>
+                  Shaping your Testimony
+                </dt>
+
+                <dd>
+                  Learning to tell your story with impact in 3-5 minutes.
+                </dd>
+
+                <dt>
+                  Heart to Heart
+                </dt>
+
+                <dd>
+                  Weekly class that includes topics such as prayer, spiritual warfare, spiritual gifts, forgiveness, and more.
+                </dd>
+
+                <dt>
+                  Anger Management
+                </dt>
+
+                <dd>
+                  A study teaching skills to handle emotions.
+                </dd>
+
+                <dt>
+                  One Thousand Gifts
+                </dt>
+
+                <dd>
+                  A Bible study on developing an attitude of gratitude.
+                </dd>
+
+                <dt>
+                  Wisdom for Mother’s
+                </dt>
+
+                <dd>
+                  A Bible study on raising you child God’s way using His practical design.
+                </dd>
+
+                <dt>
+                  Computer Skills/Job Readiness
+                </dt>
+
+                <dd>
+                  Teaches basics of Word, Excel, PowerPoint and prepares individuals for the general workforce.
+                </dd>
+
+                <dt>
+                  Special Events and Church Attendance
+                </dt>
+
+                <dd>
+                  Each week Grace House attends churches and special events in the San Antonio area as a group.
+                </dd>
+
+                <dt>
+                  Volunteering
+                </dt>
+
+                <dd>
+                  Grace House volunteers once a month with Taking it to the Streets to feed the homeless and various other activities in the community.
+                </dd>
+
+                <dt>
+                  Structured Living
+                </dt>
+
+                <dd>
+                  Learning to live in a home with a schedule, responsibilities, and accomplishments.
+                </dd>
+
+                <dt>
+                  Limited Education Scholarships
+                </dt>
+
+                <dd>
+                  When funds are available.
+                </dd>
+
+                <dt>
+                  Exit Plan
+                </dt>
+
+                <dd>
+                  Includes counseling with a number of community leaders to ensure a plan for the future. Also, a personality inventory, spiritual gifts inventory, business and resume coaching plus other resources from the community.
+                </dd>
+
+                <dt>
+                  Alumni Program
+                </dt>
+
+                <dd>
+                  On the last Sunday evening of each month, Lifeloop.live hosts an online group meeting with a special topic for encouragement and sobriety. Alumni reunion yearly.
+                </dd>
+
+                </dl> 
+            </div>
+        </div> 
+        <!-- end columns -->
+    </div>
+    <!-- end content -->  
+    
+
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
