15c91c59d39a49967d1b66ea59b69526a16b90d5
webstandardcss <webstandardcss@gmail.com>
2018-11-24 14:36:26 -0600

    blog.php

---
+++ blog.php
@@ -0,0 +1,37 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "  " . $pageTitle;
+$pageDescription = $siteName  . "  " . $pageTitle;
+$pageKeywords = $pageTitle . "  " . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+
+    <!-- blank.php -->
+    <div class="home-banner flex-centered">
+        <div class="home-banner-inner">
+            <h5>
+                &ldquo;&nbsp;&nbsp;&rdquo;
+                &mdash; 
+            </h5>
+        </div>
+    </div>
+
+    <div class="container bar-content p-centered page-width">    
+        <div class="columns">
+            <div class="column col-8 col-md-12 col-mx-auto">
+                <h2 class="text-center">Blog</h2>
+                <p>
+                    Coming Soon
+                </p>
+            </div>
+        </div> 
+        <!-- end columns -->
+    </div>
+    <!-- end content -->  
+    
+
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
