c46853f6c1c2ef074813936a2d457d5cabad7fac
webstandardcss <webstandardcss@gmail.com>
2018-11-24 05:14:57 -0600

    mission.php

---
+++ mission.php
@@ -0,0 +1,39 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "  " . $pageTitle;
+$pageDescription = $siteName  . "  " . $pageTitle;
+$pageKeywords = $pageTitle . "  " . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+
+    <!-- blank.php -->
+    <div class="home-banner flex-centered">
+        <div class="home-banner-inner">
+            <h5>
+                &ldquo;&nbsp;The LORD your God is in your midst, a mighty one who will save; he will rejoice over you with gladness; he will quiet you by his love; he will exult over you with loud singing.&nbsp;&rdquo;
+                &mdash; Zephaniah 3:17
+            </h5>
+        </div>
+    </div>
+
+    <div class="container bar-content p-centered page-width">    
+        <div class="columns">
+            <div class="column col-8 col-md-12 col-mx-auto">
+                <!-- mission.php -->
+                <h2 class="text-center">MISSION</h2>
+
+                <p>
+                    Our mission with Grace House of Longview is to provide our community with a faith based transition home for young women who have struggling with life after incarceration due to addictions and other poor living choices. Each woman will be interviewed to determine her desire to commit to 9 months of spiritual training to help lead her to a Christ centered and addiction free life.
+                </p>
+            </div>
+        </div> 
+        <!-- end columns -->
+    </div>
+    <!-- end content -->  
+    
+
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
