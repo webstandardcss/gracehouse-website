f58cf9e3acf9a93f99ddd922d481793f5ba1dbb0
webstandardcss <webstandardcss@gmail.com>
2018-11-24 15:15:23 -0600

    visitors.php

---
+++ visitors.php
@@ -0,0 +1,56 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "  " . $pageTitle;
+$pageDescription = $siteName  . "  " . $pageTitle;
+$pageKeywords = $pageTitle . "  " . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+
+    <!-- blank.php -->
+    <div class="home-banner flex-centered">
+        <div class="home-banner-inner">
+            <h5>
+                &ldquo;&nbsp;Let every person be subject to the governing authorities. For there is no authority except from God, and those that exist have been instituted by God.&nbsp;&rdquo;
+                &mdash; Romans 13:1
+            </h5>
+        </div>
+    </div>
+
+    <div class="container bar-content p-centered page-width">    
+        <div class="columns">
+            <div class="column col-8 col-md-12 col-mx-auto">
+                <h2 class="text-center">Visitor Guidelines</h2>
+
+                <p>
+                We respectfully ask that you adhere to these guideline every time you visit Grace House. We also ask that you refrain from asking for exceptions.
+                </p>
+                
+                <h4 class="text-center">GRACE HOUSE VISITOR GUIDELINES:</h4>
+
+                <ul class="gracehouse-rules">
+                  <li>Please cooperate with the staff member on duty, she is responsible for the house and she appreciates your support.</li>
+                  <li>Please leave purses, phones and laptop computers locked in your car. (Exception of laptops for teaching)</li>
+                  <li>Should you need your laptop for teaching purposes, do not allow any resident access to the internet, Facebook or other social media sites and please do not do it for them.</li>
+                  <li>NO OUTSIDE FOOD will be brought into Grace House, even for small groups, gatherings or parties. No exceptions. (This is a new rule for 2017. Our residents have an abundance of quality foods and many opportunities for culinary delights.)</li>
+                  <li>Do not bring any controlled substance on to Grace House property. This includes prescription or illegal drugs, alcoholic beverages, cigarettes, e-cigs or nicotine patches and gum.</li>
+                  <li>Do not give any Grace House resident over the counter or prescription medicine.</li>
+                  <li>Please stay in the common areas of Grace House, no one should enter the resident bedrooms.</li>
+                </ul>
+
+                <p>Your generosity and heart to help is always appreciated but never expected. If you wish to give something to the residents when visiting, please consider small gifts for all 12 ladies,</p>
+
+                <p>These are the acceptable items:   nail polish, chap-stick, lipstick, eye shadow, face powder, eye liners, socks, gloves, flip-flops, house shoes, refillable water bottles, combs, brushes, hair bands, hair ties or clips, pens, pencils, erasers, highlighters, journals, spiral notebooks, file folders, sticky notes, bookmarks, small jewelry, sweat shirts, T shirts, or other small gift items that have prior approval from a staff member.</p>
+
+                <p>Thank you so much for your cooperation with this matter.</p>
+            </div>
+        </div> 
+        <!-- end columns -->
+    </div>
+    <!-- end content -->  
+    
+
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
