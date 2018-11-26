2f6b8f712c2709d96b08f55c436062e31e75bda3
webstandardcss <webstandardcss@gmail.com>
2018-11-25 22:29:27 -0600

    add a divider to make the menu look good

--- inc/_nav_menu_program.php
+++ inc/_nav_menu_program.php
@@ -6,6 +6,7 @@
                             <i class="icon icon-link"></i> Our Program
                             </a>
                         </li>
+                        <li class="divider"></li>
                         <li class="menu-item">
                             <a href="/education.php">
                             <i class="icon icon-link"></i> Education and Recovery Classes
