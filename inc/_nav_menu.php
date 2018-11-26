3e26dea05c371b5f2007faca060665eb99d1570c
webstandardcss <webstandardcss@gmail.com>
2018-11-25 21:59:01 -0600

    add Our Program link to the dropdown menu

--- inc/_nav_menu.php
+++ inc/_nav_menu.php
@@ -37,6 +37,11 @@
                     <ul class="menu">
                         <li class="divider" data-content="OUR PROGRAM"></li>
                         <li class="menu-item">
+                            <a href="/our-program.php">
+                            <i class="icon icon-link"></i> Our Program
+                            </a>
+                        </li>
+                        <li class="menu-item">
                             <a href="/education.php">
                             <i class="icon icon-link"></i> Education and Recovery Classes
                             </a>
