1a8b5bb99ff48d051eace32cf1a06619541fd190
webstandardcss <webstandardcss@gmail.com>
2018-12-04 06:52:51 +0000

    fix menu

--- inc/_nav_menu_alternate.php
+++ inc/_nav_menu_alternate.php
@@ -1,6 +1,6 @@
 
 <a href="/our-story.php" class="btn btn-link" tabindex="0">Our Story</a>
-<a href="/our-mission.php" class="btn btn-link" tabindex="1">Mission</a>
+<a href="/mission.php" class="btn btn-link" tabindex="1">Mission</a>
 <a href="/our-program.php" class="btn btn-link" tabindex="2">Our Program</a>
 <a href="/volunteer.php" class="btn btn-link" tabindex="3">Volunteer</a>
 <a href="/donate.php" class="btn btn-link" tabindex="4">Donate</a>
